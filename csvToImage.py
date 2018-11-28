4d8d4b189c169d929a20085e8f0fa993b380d84b
Dongming Zhang <dzhang123@gmail.com>
2018-11-27 21:20:44 -0600

    update title in png file

--- csvToImage.py
+++ csvToImage.py
@@ -9,7 +9,8 @@ def ToImage(csv):
         df = pd.read_csv(csv, comment='#')
         plt.close()
         plt.scatter(df.length, df.weight) #, marker='.')
-        plt.title(csv.replace('.csv', '').replace('_', ''))
+        #plt.title((csv.rsplit('/')[0]).replace('.csv', '').replace('_', ''))
+        plt.title((csv.rsplit('/', 1)[1]).replace('.csv', '').replace('_', ' '))
         plt.savefig(csv.replace('.csv', '.png'))
 
 if __name__ == '__main__':
