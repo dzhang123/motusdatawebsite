9a59de41ae9ece962125336d2bc491d935eb6785
Dongming Zhang <dzhang123@gmail.com>
2018-12-18 13:20:15 -0600

    added error handling to index

--- controllers/dynacardController.js
+++ controllers/dynacardController.js
@@ -15,11 +15,12 @@ var async = require('async');
 // GET request.
 // 1. if any csv file exists in /public/uploads/ folder, call python to generate png (for now) to display and then csv file and upload/store the zip file in mongoodb. Once stored, delete the csv file
 // 2. if none, retrieve csv files from mongoodb, call python to create png file to display.
-exports.index = (req, res) => {
+exports.index = (req, res, next) => {
     //res.render('index', {title: 'Dynacard List'});
     Dynacard.find({})
     .populate('cardtype')
     .exec((err, list_cards) => {
+        if (err) { return next (err);}
         res.render('index', {title: 'Dyncards Home', error: err, dynacards: list_cards});
     });
 };
